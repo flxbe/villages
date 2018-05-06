f05a9c894bf901651dc1076ef653e2fb0e80650a
Felix Bernhardt <felixbernhardt@yahoo.com>
2018-05-06 16:03:04 +0200

    [#33] add more tests for astar

--- app/mock-server/astar.test.js
+++ app/mock-server/astar.test.js
@@ -1,6 +1,6 @@
 import astar from "./astar.js";
 import * as Constants from "../constants.js";
-import { getTileCenter } from "../util.js";
+import { getTileCenter, cart2tile, isWalkableTile } from "../util.js";
 
 const { expect } = chai;
 
@@ -26,12 +26,29 @@ describe("astar", () => {
     ]
   ];
 
-  it("should compute a path from 'start' to 'target'", () => {
+  it("path should start at 'start'", () => {
     const path = astar(map, start, target);
-    console.log(path);
     const pathStart = path[0];
     expect([pathStart.x, pathStart.y]).to.deep.equal(
       getTileCenter(start[0], start[1])
     );
+  });
+
+  it("path should end at 'target'", () => {
+    const path = astar(map, start, target);
+    const pathEnd = path[path.length - 1];
+    expect([pathEnd.x, pathEnd.y]).to.deep.equal(
+      getTileCenter(target[0], target[1])
+    );
+  });
+
+  it("path should only include walkable tiles", () => {
+    const path = astar(map, start, target);
+    expect(
+      path.every(node => {
+        const [i, j] = cart2tile(node.x, node.y);
+        return isWalkableTile(map[i][j].type);
+      })
+    ).to.be.true;
   });
 });
