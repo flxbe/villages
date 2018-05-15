6ed0561835d528f15b03a9ed8dd88f3f04c00e68
Felix Bernhardt <felixbernhardt@yahoo.com>
2018-05-15 15:02:29 +0200

    more app/util.js cleanup

--- app/util.js
+++ app/util.js
@@ -1,6 +1,3 @@
-import * as Constants from "../common/constants.js";
-import context from "./context.js";
-
 /**
  * Convert decimal number to hexadecimal string.
  * @param {number} n - expected to be in {0,...,255}
@@ -29,17 +26,4 @@ export function rgb2hexColor(r, g, b) {
  */
 export function shade2hexColor(n) {
   return rgb2hexColor(n, n, n);
-}
-
-/**
- *
- * @param {number} x - hitbox x
- * @param {number} y - hitbox y
- * @param {number} w - hitbox width
- * @param {number} h - hitbox height
- * @param {number} px - point x
- * @param {number} py - point y
- */
-export function pointInHitbox(x, y, w, h, px, py) {
-  return px >= x && px <= x + w && py >= y && py <= y + h;
 }
